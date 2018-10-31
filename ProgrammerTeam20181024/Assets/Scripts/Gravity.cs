c6d83f9831af6328fb1a7741ea12c92c6b10401d
NottyAce <39573156+NottyAce@users.noreply.github.com>
2018-10-31 19:14:10 +0900

    重力と惑星作成中

---
+++ ProgrammerTeam20181024/Assets/Scripts/Gravity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+
+    Vector3 localGravity;
+   
+    Rigidbody rb;
+    
+    // Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        SetLocalGravity();   
+    }
+
+    void SetLocalGravity()
+    {
+        rb.AddForce(localGravity, ForceMode.Acceleration);
+    }
+
+    private void GravityController()
+    {
+       
+    }
+}
