4d2385d3c837ad6797d83b6157ebae3bb04b46b8
momitatsu3 <momitatsu3@gmail.com>
2018-11-28 19:23:03 +0900

    うまぴょい伝説歌詞
    うまぴょい伝説の歌詞実装

---
+++ ProgrammerTeam20181024/Assets/Scenes/momiyama/SendText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendText : MonoBehaviour {
+
+    [SerializeField]
+    List<string> messages;
+
+    [SerializeField]
+    UnityEngine.UI.Text messageBox;
+
+    private List<string>.Enumerator enumerator;
+
+    private int current = 0;
+
+    // Use this for initialization
+    void Start () {
+        enumerator = messages.GetEnumerator();
+
+        OnClick();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnClick();
+        }
+
+	}
+
+    public void OnClick()
+    {
+        if (enumerator.MoveNext())
+            messageBox.text = enumerator.Current;
+    }
+}
