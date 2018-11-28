340c3e6a9b7eb831f21ee2904e45035363c33ca7
sota yamamoto <44431841+yamaso@users.noreply.github.com>
2018-11-28 19:18:08 +0900

    yamaso/send_text

---
+++ ProgrammerTeam20181024/Assets/Send_text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Send_text : MonoBehaviour
+{
+
+    [SerializeField] UnityEngine.UI.Text textbox;
+    IEnumerator Start()
+    {
+        textbox.text ="おはよう";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text ="お腹すいた";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text = "おやすみ";
+    }
+}
