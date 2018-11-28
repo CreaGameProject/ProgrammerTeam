4ed33adf117544c1311aaf55011c901e219a75cc
NottyAce <39573156+NottyAce@users.noreply.github.com>
2018-11-28 19:26:56 +0900

    null出現中

---
+++ ProgrammerTeam20181024/Assets/TalkingText.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkingText : MonoBehaviour {
+
+    Text textEdit;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(TextController());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator TextController()
+    {
+        textEdit.text = "あっ！";
+        Debug.Log(1);
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textEdit.text = "明後日のPl会議の議長俺や！";
+        Debug.Log(2);
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textEdit.text = "カオスなことになりそう \n 楽しみやなあ～～";
+        Debug.Log(3);
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        yield break;
+    }
+
+
+
+
+
+
+
+}
