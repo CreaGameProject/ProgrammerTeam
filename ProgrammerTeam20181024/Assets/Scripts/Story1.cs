f57055326c0b7e7d28fa8b83009375591a8dc074
FukuchiYuki <rkrn.zerozerozerooox@gmail.com>
2018-11-28 19:14:53 +0900

    テキストの表示

---
+++ ProgrammerTeam20181024/Assets/Scripts/Story1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Story1 : MonoBehaviour
+{
+    [SerializeField] Text textBox;
+
+    IEnumerator Start()
+    {
+        textBox.text = "衝撃のファーストブリット";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textBox.text = "撃滅のセカンドブリット";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textBox.text = "抹殺のラストブリット";
+    }
+}
