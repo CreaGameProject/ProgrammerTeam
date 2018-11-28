3c2d2a3eced04168c79c87492dc54ac82f5ad7c9
Kazuya-Pum <kazuya.pum@gmail.com>
2018-11-28 19:01:24 +0900

    テキスト切り替え実装

---
+++ ProgrammerTeam20181024/Assets/SendingText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendingText : MonoBehaviour {
+
+    IEnumerator Start()
+    {
+        Debug.Log( "衝撃のファーストブリット" );
+        yield return new WaitUntil( () => Input.GetMouseButtonDown( 0 ) );
+        yield return null;
+
+        Debug.Log( "撃滅のセカンドブリット" );
+        yield return new WaitUntil( () => Input.GetMouseButtonDown( 0 ) );
+        yield return null;
+
+        Debug.Log( "抹殺のラストブリット" );
+    }
+}
