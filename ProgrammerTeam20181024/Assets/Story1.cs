926ac636d9152b40030edf8d9d6b607b28432c7b
fujitaiori416 <ggrfeyona1999@gmail.com>
2018-11-28 19:28:33 +0900

    文字を表示

---
+++ ProgrammerTeam20181024/Assets/Story1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Story1 : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Text textbox;
+
+    IEnumerator Start()
+    {
+        textbox.text = "衝撃のファーストブリット";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text = "撃滅のセカンドブリット";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text = "抹殺のラストブリット";
+    }
+}
