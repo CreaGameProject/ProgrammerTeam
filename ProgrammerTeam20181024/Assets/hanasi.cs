ff32371a3fafa7763e3882098363f8a221f26934
sugizaki35 <kaikai484848@gmail.com>
2018-11-28 19:20:34 +0900

    何とか

---
+++ ProgrammerTeam20181024/Assets/hanasi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hanasi : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Text textbox;
+
+    IEnumerator Start()
+    {
+        textbox.text = ("飯");
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text = ("食べる");
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text = ("うまい");
+    
+    }
+}
