a4abd11ceeb5fa5678d84381b80dd2698a0c9750
fujitaiori416 <ggrfeyona1999@gmail.com>
2018-12-05 19:45:22 +0900

    表情変化

--- ProgrammerTeam20181024/Assets/Story1.cs
+++ ProgrammerTeam20181024/Assets/Story1.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Story1 : MonoBehaviour
 {
     [SerializeField] UnityEngine.UI.Text textbox;
+    [SerializeField] SpriteRenderer body, face;
+
+    [SerializeField] SpriteAtlas atlas;
 
     IEnumerator Start()
     {
         textbox.text = "衝撃のファーストブリット";
+        body.sprite = atlas.GetSprite("サンプル1");
+        //face.sprite = atlas.GetSprite("kohaku_1");
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         yield return null;
 
+        face.sprite = atlas.GetSprite("サンプル");
         textbox.text = "撃滅のセカンドブリット";
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         yield return null;
 
+        // face.sprite = atlas.GetSprite("kohaku_3");
+        face.sprite = null;
         textbox.text = "抹殺のラストブリット";
     }
 }
