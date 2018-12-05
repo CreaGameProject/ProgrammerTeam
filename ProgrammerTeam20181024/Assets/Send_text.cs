1f5532210e285f52edc77de7ef6db52209bdb693
sota yamamoto <44431841+yamaso@users.noreply.github.com>
2018-12-05 19:06:10 +0900

    sprite

--- ProgrammerTeam20181024/Assets/Send_text.cs
+++ ProgrammerTeam20181024/Assets/Send_text.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Send_text : MonoBehaviour
 {
 
     [SerializeField] UnityEngine.UI.Text textbox;
+    [SerializeField] SpriteRenderer body, face;
+
+    [SerializeField] SpriteAtlas atlas;
     IEnumerator Start()
     {
         textbox.text ="おはよう";
+        body.sprite = atlas.GetSprite("maki_body");
+        face.sprite = atlas.GetSprite("maki_1");
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         yield return null;
 
+        face.sprite = atlas.GetSprite("maki_2");
         textbox.text ="お腹すいた";
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         yield return null;
 
+        face.sprite = atlas.GetSprite("maki_3");
         textbox.text = "おやすみ";
     }
 }
