e032dda326566bfb7e69d7c4553bd38548636088
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2018-12-05 19:53:46 +0900

    差分

--- ProgrammerTeam20181024/Assets/Story1.cs
+++ ProgrammerTeam20181024/Assets/Story1.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class Story1 : MonoBehaviour
 {
     [SerializeField] UnityEngine.UI.Text textbox;
+    [SerializeField] Image body, face;
+
+    [SerializeField] SpriteAtlas atlas;
 
     IEnumerator Start()
     {
         textbox.text = "衝撃のファーストブリット";
+        body.sprite = atlas.GetSprite("sample");
+        face.sprite = atlas.GetSprite("sample2");
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         yield return null;
 
+        face.sprite = atlas.GetSprite("sample3");
         textbox.text = "撃滅のセカンドブリット";
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
         yield return null;
 
+        face.sprite = atlas.GetSprite("sample4");
         textbox.text = "抹殺のラストブリット";
     }
 }
