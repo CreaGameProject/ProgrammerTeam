1f4e5558042e20dac8f7ec15f8c9c47b587be44a
Kazuya-Pum <kazuya.pum@gmail.com>
2018-12-05 19:50:23 +0900

    表情変更を実装

--- ProgrammerTeam20181024/Assets/SendingText.cs
+++ ProgrammerTeam20181024/Assets/SendingText.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.U2D;
 
-public class SendingText : MonoBehaviour {
+public class SendingText : MonoBehaviour
+{
 
     [SerializeField] private Text text;
-    [Header("int"), SerializeField] int a;
     [SerializeField] private List<string> msg;
     private List<string>.Enumerator enumerator;
-    [HideInInspector] public string b;
+
+    [SerializeField] Image body, face;
+
+    [SerializeField] SpriteAtlas atlas;
+
 
     IEnumerator Start()
     {
+        //body.sprite = atlas.GetSprite( "base" );
+
         enumerator = msg.GetEnumerator();
 
         yield return StartCoroutine( "OnClick" );
@@ -34,10 +41,14 @@ public class SendingText : MonoBehaviour {
 
         text.text = "";
 
+        face.sprite = atlas.GetSprite( "sample_4" );
         foreach ( char item in enumerator.Current )
         {
+            face.gameObject.SetActive( true );
             text.text += item;
-            yield return new WaitForSeconds( 0.1f );
+            yield return new WaitForSeconds( 0.075f );
+            face.gameObject.SetActive( false );
+            yield return new WaitForSeconds( 0.025f );
         }
 
         yield return new WaitUntil( () => Input.GetMouseButtonDown( 0 ) );
