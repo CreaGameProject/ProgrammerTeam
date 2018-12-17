fa2bb17cdbaa73422c8ee12c86d63fc92dbffd67
FukuchiYuki <rkrn.zerozerozerooox@gmail.com>
2018-12-17 23:15:37 +0900

    とりあえず見た目調整とメッセージを表示

---
+++ ProgrammerTeam20181024/Assets/Scripts/Story.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Story : MonoBehaviour
+{
+    [SerializeField] List<string> messages;
+    [SerializeField] Text text;
+    private List<string>.Enumerator enumerator;
+    //private string firstText;
+
+    void Start()
+    {
+        enumerator = messages.GetEnumerator();
+        text.text = messages[0];
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && enumerator.MoveNext())
+        {
+            text.text = enumerator.Current;
+        }
+    }
+}
