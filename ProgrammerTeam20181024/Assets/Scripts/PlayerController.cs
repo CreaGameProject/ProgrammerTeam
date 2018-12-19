4776b090b4e136f7e407951598fa5137aed49ab0
Kazuya-Pum <kazuya.pum@gmail.com>
2018-12-19 16:15:04 +0900

    y座標によってサイズを変更

---
+++ ProgrammerTeam20181024/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform tfCache;
+    [SerializeField] private Transform centerPosition;
+    private Vector3 centerPos;
+    private Vector3 mousePos;
+
+    [SerializeField] private float limit;
+
+    void Start()
+    {
+        tfCache = transform;
+        centerPos = centerPosition.localPosition;
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        Debug.Log( tfCache.position + centerPos );
+        tfCache.position = Vector3.MoveTowards( tfCache.position, mousePos, 0.05f ) - centerPos;
+        //if(Mathf.Abs( centerPosition.position.x) < limit &&)
+    }
+}
---
+++ ProgrammerTeam20181024/Assets/Scripts/SizeChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChanger : MonoBehaviour
+{
+
+    private Transform tfCache;
+    [SerializeField] private float amount;
+    private Vector3 scale;
+    private float defaultScale;
+    private float changedScale;
+
+    void Start()
+    {
+        tfCache = transform;
+        scale = tfCache.localScale;
+        defaultScale = tfCache.localScale.x;
+    }
+
+    void Update()
+    {
+        changedScale = defaultScale + Mathf.Abs( tfCache.localPosition.y ) * amount;
+        scale.Set( changedScale, changedScale, 1 );
+        tfCache.localScale = scale;
+    }
+}
