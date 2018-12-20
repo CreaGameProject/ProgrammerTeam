9120a04ede0217dfd86ef8c6389569fc70547de0
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2018-12-20 17:50:59 +0900

    データーベース構築 アルファ版

---
+++ anima/Assets/GunDataBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunDataBase", menuName = "CreateGunDataBase")]
+public class GunDataBase : ScriptableObject
+{
+    [SerializeField] private List<Gun> gunLists = new List<Gun>();
+
+    public List<Gun> GetGunLists()
+    {
+        return gunLists;
+    }
+}
