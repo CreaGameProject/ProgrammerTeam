c122e4b6130d5c3daa9b080f4c2177d7fb736212
OmoriItsuki <39805387+kinoko555@users.noreply.github.com>
2018-12-22 20:19:28 +0900

    データベース構築

---
+++ anima/Assets/Gun.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[SerializeField] [CreateAssetMenu(fileName = "Gun", menuName = "CreateGun")]
+
+
+public class Gun : ScriptableObject {
+
+    //銃の種類の登録
+    public enum KindOfGun
+    {
+        SniperRifle=1,
+        ShotGun
+    }
+
+    
+    //銃の種類の選択
+    [SerializeField] private KindOfGun kindOfGun;
+
+    [SerializeField] private bool single;
+  
+    //銃の名前
+    [SerializeField] private string gunName;
+
+    //銃の射程
+    [SerializeField] private int range;
+
+    //銃の基礎威力
+    [SerializeField] private int power;
+    
+    //リロード時間
+    [SerializeField] private int relordTime;
+
+    //最大所持段数
+    [SerializeField] private int bringBullets;
+
+    //最大装填弾数
+    [SerializeField] private int maxBullets;
+
+    //以下　銃の情報呼び出し
+    public KindOfGun GetKindOfGun()
+    {
+        return kindOfGun;
+    }
+    public string GetGunName()
+    {
+        return gunName;
+    }
+    public int GetRange()
+    {
+        return range;
+    }
+    public int GetPower()
+    {
+        return power;
+    }
+    public bool GetSingle()
+    {
+        return single;
+    }
+    
+}
---
+++ anima/Assets/GunManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunManager : MonoBehaviour {
+
+    [SerializeField] private GunDataBase gunDataBase;
+
+    private Dictionary<Gun, int> numOfGun = new Dictionary<Gun, int>();
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < gunDataBase.GetGunLists().Count; i++)
+        {
+            numOfGun.Add(gunDataBase.GetGunLists()[i], i);
+            Debug.Log(gunDataBase.GetGunLists()[i].GetGunName());
+
+        }
+
+	}
+	
+	// Update is called once per frame
+	public Gun GetItem(string searchName)
+    {
+        return gunDataBase.GetGunLists().Find(gunName => gunName.GetGunName() == searchName);
+    }
+}
