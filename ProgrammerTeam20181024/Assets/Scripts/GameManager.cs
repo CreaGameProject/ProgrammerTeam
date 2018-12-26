96b815dccf18a79a5ed706e1217c096158bbdb75
Kazuya-Pum <kazuya.pum@gmail.com>
2018-12-26 18:21:35 +0900

    成功判定

--- ProgrammerTeam20181024/Assets/Scripts/GameManager.cs
+++ ProgrammerTeam20181024/Assets/Scripts/GameManager.cs
@@ -2,13 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform ball;
+    [SerializeField] private Transform player;
+    private float diffY;
+    private float diffX;
+    private float meet;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if ( Input.GetMouseButtonDown( 0 ) )
+        {
+            Swing();
+        }
+    }
+
+    private void Swing()
+    {
+        diffY = ball.position.y - player.position.y;
+
+        if(Mathf.Abs(diffY) > 1 )
+        {
+            return;
+        }
+
+        diffX = ball.position.x - player.position.x;
+
+        if(Mathf.Abs(diffX) > meet )
+        {
+            return;
+        }
+    }
 }
