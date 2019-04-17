8abddbb5b7888cb40438635c03b2142565248d85
Lycolet <yusukenakano16@gmail.com>
2019-04-18 03:54:06 +0900

    がんばった

---
+++ ProgrammerTeam20181024/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject target;//プレイヤーオブジェクト指定
+
+    NavMeshAgent navMeshAgent;//インスタンス格納用変数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.transform.position);//Vecter3型で移動先の座標を指定
+    }
+}
---
+++ ProgrammerTeam20181024/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 curPos = transform.position;
+
+        float moveLength = speed * Time.smoothDeltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            curPos.x -= moveLength;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            curPos.x += moveLength;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            curPos.z += moveLength;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            curPos.z -= moveLength;
+        }
+        navMeshAgent.SetDestination(curPos);
+    }
+}
